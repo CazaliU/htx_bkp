54efe3c85d21ba5e218a9f5f861587632b04dcf1
CazaliU <rafaelcazali@gmail.com>
2025-02-24 17:55:24 -0300

    insecao banco veiculo"

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from sqlalchemy.orm import sessionmaker, session
 from selenium.webdriver.common.by import By
 from sqlalchemy.exc import IntegrityError
-from sql import engine, VeiculosIntegrantes, DadosIntegrantes
-from sqlalchemy.orm import sessionmaker, session
+from sql import engine, Veiculos
+from dotenv import load_dotenv
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pyautogui
-from dotenv import load_dotenv
 import time
 import os
 
@@ -135,17 +135,6 @@ try:
         elif texto.startswith("Índice de Participação:"):
             indice_participacao = texto.replace("Índice de Participação:", "").strip()
 
-    print(f"Integrante: {integrante}")
-    print(f"Tipo: {tipo}")
-    print(f"Espécie: {especie}")
-    print(f"Composição: {composicao}")
-    print(f"Cod. Fipe: {cod_fipe}")
-    print(f"Valor Principal: {valor_principal}")
-    print(f"Agregado: {agregado}")
-    print(f"Índice de Participação: {indice_participacao}")
-
-    print("--------------------------------------")
-    
     # Inicializa dicionário para armazenar os valores
     dados_veiculo = {
         "marca": [],
@@ -159,116 +148,16 @@ try:
         "estado": [],
         "cidade": [],
         "documento": [],
-        "espécie": [],
-        "tipo": [],
-        "carroceria": [],
-        "Cap. Max. Carga": [],
-        "Peso Bruto Total": [],
-        "Cap. Max. Tração": [],
-        "N°. Motor": [],
-        "potência": [],
-        "lotação": [],
-        "eixos": [],
-        "Nº. CRV": [],
-        "Nº. Seg. CLA": [],
-        "Observações": [],
-        "rastreadores": [],
-        "bloqueadores": [],
-        "Última Vistoria": [],
-        "monitoramento": [],
-        "Anotações de Controle:": []
     }
 
-    # Percorre todos os elementos para capturar as informações
-    for elemento in elementos:
-        texto = elemento.text.strip()
-        if texto.startswith("Cap. Max. Carga:"):
-            cap_max_carga = texto.replace("Cap. Max. Carga:", "").strip()
-            dados_veiculo["Cap. Max. Carga"].append(cap_max_carga)
-        elif texto.startswith("Peso Bruto Total:"):
-            peso_bruto_total = texto.replace("Peso Bruto Total:", "").strip()
-            dados_veiculo["Peso Bruto Total"].append(peso_bruto_total)
-        elif texto.startswith("Cap. Max. Tração:"):
-            cap_max_tracao = texto.replace("Cap. Max. Tração:", "").strip()
-            dados_veiculo["Cap. Max. Tração"].append(cap_max_tracao)
-        elif texto.startswith("N°. Motor:"):
-            numero_motor = texto.replace("N°. Motor:", "").strip()
-            dados_veiculo["N°. Motor"].append(numero_motor)
-        elif texto.startswith("Nº. CRV:"):
-            numero_crv = texto.replace("Nº. CRV:", "").strip()
-            dados_veiculo["Nº. CRV"].append(numero_crv)
-        elif texto.startswith("Nº. Seg. CLA:"):
-            numero_seg_cla = texto.replace("Nº. Seg. CLA:", "").strip()
-            dados_veiculo["Nº. Seg. CLA"].append(numero_seg_cla)
-        elif texto.startswith("Observações:"):
-            observacoes = texto.replace("Observações:", "").strip()
-            dados_veiculo["Observações"].append(observacoes)
-        elif texto.startswith("Rastreadores:"):
-            rastreadores = texto.replace("Rastreadores:", "").strip()
-            span_tag = elemento.find('span')
-            if span_tag:
-                rastreadores += " " + span_tag.get_text(strip=True)
-            dados_veiculo["rastreadores"].append(rastreadores)
-        elif texto.startswith("Bloqueadores:"):
-            bloqueadores = texto.replace("Bloqueadores:", "").strip()
-            span_tag = elemento.find('span')
-            if span_tag:
-                bloqueadores += " " + span_tag.get_text(strip=True)
-            dados_veiculo["bloqueadores"].append(bloqueadores)
-        elif texto.startswith("Última Vistoria:"):
-            ultima_vistoria = texto.replace("Última Vistoria:", "").strip()
-            dados_veiculo["Última Vistoria"].append(ultima_vistoria)
-        elif texto.startswith("Monitoramento:"):
-            monitoramento = texto.replace("Monitoramento:", "").strip()
-            span_tag = elemento.find('span')
-            if span_tag:
-                monitoramento = span_tag.get_text(strip=True)
-            dados_veiculo["monitoramento"].append(monitoramento)
-        elif texto.startswith("Anotações de Controle:"):
-            anotacoes_controle = elemento.find('div', class_='well').text.strip()
-            dados_veiculo["Anotações de Controle:"].append(anotacoes_controle)
-        else:
-            label_tag = elemento.find('b')
-            if label_tag:
-                label = label_tag.get_text(strip=True).replace(":", "")
-                valor = label_tag.next_sibling.strip() if label_tag.next_sibling else ""
-                if label.lower() in dados_veiculo:
-                    dados_veiculo[label.lower()].append(valor)
 
-    # Exibe os resultados
-    print("Marcas encontradas:", dados_veiculo["marca"])
-    print("Modelos encontrados:", dados_veiculo["modelo"])
-    print("Placas encontradas:", dados_veiculo["placa"])
-    print("Ano de Fabricação encontrados:", dados_veiculo["ano fabricação"])
-    print("Ano de Modelo encontrados:", dados_veiculo["ano modelo"])
-    print("Renavam encontrados:", dados_veiculo["renavam"])
-    print("Chassi encontrados:", dados_veiculo["chassi"])
-    print("Cores encontradas:", dados_veiculo["cor"])
-    print("Estados encontrados:", dados_veiculo["estado"])
-    print("Cidades encontradas:", dados_veiculo["cidade"])
-    print("Documentos encontrados:", dados_veiculo["documento"])
-    print("Espécies encontrados:", dados_veiculo["espécie"])
-    print("Tipos encontrados:", dados_veiculo["tipo"])
-    print("Carrocerias encontradas:", dados_veiculo["carroceria"])
-    print("Capacidade Máxima de Carga encontradas:", dados_veiculo["Cap. Max. Carga"])
-    print("Peso Bruto Total encontradas:", dados_veiculo["Peso Bruto Total"])
-    print("Capacidade Máxima de Tração encontradas:", dados_veiculo["Cap. Max. Tração"])
-    print("Número do Motor encontrados:", dados_veiculo["N°. Motor"])
-    print("Potência encontradas:", dados_veiculo["potência"])
-    print("Lotação encontradas:", dados_veiculo["lotação"])
-    print("Eixos encontrados:", dados_veiculo["eixos"])
-    print("Número do CRV encontrados:", dados_veiculo["Nº. CRV"])
-    print("Número do Seguro Classe encontrados:", dados_veiculo["Nº. Seg. CLA"])
-    print("Observações encontradas:", dados_veiculo["Observações"])
-    print("Rastreadores encontrados:", dados_veiculo["rastreadores"])
-    print("Bloqueadores encontrados:", dados_veiculo["bloqueadores"])
-    print("Última Vistoria encontradas:", dados_veiculo["Última Vistoria"])
-    print("Monitoramento encontradas:", dados_veiculo["monitoramento"])
-    print("Anotações de Controle encontradas:", dados_veiculo["Anotações de Controle:"])
-    print("--------------------------------------")
-
-
-
+    label_tag = elemento.find('b')
+    if label_tag:
+        label = label_tag.get_text(strip=True).replace(":", "")
+        valor = label_tag.next_sibling.strip() if label_tag.next_sibling else ""
+        if label.lower() in dados_veiculo:
+            dados_veiculo[label.lower()].append(valor)
+            
 
 except Exception as e:
     print(f"Erro ao localizar os dados: {e}")
