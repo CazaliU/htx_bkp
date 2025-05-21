bb15bb84955ae318b97e0b74816fad704314d341
CazaliU <rafaelcazali@gmail.com>
2025-05-21 15:00:09 -0300

    anexos prontos

--- sinistros.py
+++ sinistros.py
@@ -21,23 +21,47 @@ import re
 import time
 import os
 
-def save_to_database(session, cliente_id, veiculo_1, veiculo_2, veiculo_3, estado, cidade, tipo, codigo, responsabilidade, descricao_privada, descricao_publica, data_ocorrencia, status):
-    try:
-        # Substitui strings vazias por None
-        veiculo_1 = veiculo_1 if veiculo_1 and veiculo_1.strip() else None
-        veiculo_2 = veiculo_2 if veiculo_2 and veiculo_2.strip() else None
-        veiculo_3 = veiculo_3 if veiculo_3 and veiculo_3.strip() else None
-        estado = estado if estado.strip() else None
-        cidade = cidade if cidade.strip() else None
-        tipo = tipo if tipo.strip() else None
-        codigo = codigo if codigo.strip() else None
-        responsabilidade = responsabilidade if responsabilidade.strip() else None
-        descricao_privada = descricao_privada if descricao_privada.strip() else None
-        descricao_publica = descricao_publica if descricao_publica.strip() else None
-        data_ocorrencia = data_ocorrencia if data_ocorrencia.strip() else None
-        status = status if status.strip() else None
+remote_folder = "/var/www/imagens_sinistros/"
 
-        # Cria um único registro para o sinistro
+def process_images_and_save(session, cliente_id, veiculo_1, veiculo_2, veiculo_3, estado, cidade, tipo, codigo, responsabilidade, descricao_privada, descricao_publica, data_ocorrencia, status, image_links, nomes_imagens):
+    local_folder = f"temp_sinistro_{codigo}"  # Pasta temporária para armazenar as imagens
+    remote_paths = []
+
+    try:
+        # Cria a pasta temporária
+        if not os.path.exists(local_folder):
+            os.makedirs(local_folder)
+
+        # Baixa todas as imagens localmente e renomeia
+        for href, novo_nome in image_links:
+            local_path = os.path.join(local_folder, novo_nome)
+
+            # Baixa a imagem
+            response = requests.get(href, stream=True)
+            if response.status_code == 200:
+                with open(local_path, 'wb') as file:
+                    for chunk in response.iter_content(1024):
+                        file.write(chunk)
+                print(f"Imagem baixada e renomeada: {local_path}")  # Agora o nome correto será exibido
+            else:
+                print(f"Erro ao baixar a imagem: {href}")
+
+        # Envia todas as imagens para o servidor
+        transport = paramiko.Transport((host_ssh, port_ssh))
+        transport.connect(username=username_ssh, password=password_ssh)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+
+        for file_name in os.listdir(local_folder):
+            local_path = os.path.join(local_folder, file_name)
+            remote_path = os.path.join(remote_folder, file_name)
+            sftp.put(local_path, remote_path)
+            remote_paths.append(remote_path)  # Salva o caminho completo no banco
+            print(f"Imagem enviada para o servidor: {remote_path}")
+
+        sftp.close()
+        transport.close()
+
+        # Salva os dados do sinistro no banco de dados
         novo_sinistro = Sinistros(
             si_cliente_id=cliente_id,
             si_veiculo_1=veiculo_1,
@@ -51,14 +75,25 @@ def save_to_database(session, cliente_id, veiculo_1, veiculo_2, veiculo_3, estad
             si_descricao_privada=descricao_privada,
             si_descricao_publica=descricao_publica,
             si_data_ocorrencia=data_ocorrencia,
-            si_status=status
+            si_status=status,
+            si_caminho_anexos=remote_paths  # Salva os caminhos completos no banco
         )
         session.add(novo_sinistro)
         session.commit()
-        print("Dados do sinistro salvos no banco de dados com sucesso!")
+        print("Dados do sinistro e caminhos das imagens salvos no banco de dados com sucesso!")
+
     except Exception as e:
-        print(f"Erro ao salvar no banco de dados: {e}")
+        print(f"Erro ao processar as imagens: {e}")
         session.rollback()
+    finally:
+        # Remove a pasta temporária e os arquivos locais
+        if os.path.exists(local_folder):
+            try:
+                print(f"Removendo a pasta temporária: {local_folder}")
+                shutil.rmtree(local_folder)
+                print("Pasta temporária removida com sucesso.")
+            except Exception as e:
+                print(f"Erro ao remover a pasta temporária: {e}")
 
 
 # Obter as credenciais a partir das variáveis de ambiente
@@ -70,7 +105,6 @@ host_ssh = os.getenv('HOST_SSH')
 port_ssh = int(os.getenv('PORT_SSH'))  # Converte para inteiro
 username_ssh = os.getenv('USERNAME_SSH')
 password_ssh = os.getenv('PASSWORD_SSH')
-# remote_folder = "/var/www/imagens_vistorias/"
 
 # Configura o caminho para o ChromeDriver
 chrome_driver_path = r'C:\Users\rafae\Downloads\chromedriver-win64\chromedriver.exe' 
@@ -230,9 +264,57 @@ for index, botao in enumerate(botoes_ver):
                 finally:
                     session.close()
 
-            # Salva os dados no banco de dados
+        else:
+            print("Dados do sinistro não encontrados.")
+
+
+        # Localiza a aba anexos dentro do modal
+        aba_anexos = driver.find_element(By.CSS_SELECTOR, 'li[data-id="anexos"]')
+        aba_anexos.click()
+        time.sleep(2)  # Aguarde o carregamento da aba
+        
+        # Extrai o HTML da página
+        html = driver.page_source
+        # Analisa o HTML com BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        # URL base do site para transformar os links relativos em absolutos
+        base_url = "https://www.hitex.com.br"
+        
+        # Localiza o contêiner com os anexos
+        anexos_container = soup.find('div', class_='anexos_previews')
+
+        # Captura os links das imagens dentro do contêiner de anexos
+        if anexos_container:
+            links = anexos_container.find_all('a', class_='abrir_anexo')
+            imagens = []
+            nomes_imagens = []
+
+            for link in links:
+                href = urljoin(base_url, link['href'])  # Gera o link completo
+                if href:
+                    # Extrai o identificador do parâmetro "a="
+                    identificador = re.search(r'a=([^&]+)', href).group(1)  # Captura o valor após "a="
+                    
+                    # Verifica se o identificador já contém "uni_"
+                    if not identificador.startswith("uni_"):
+                        novo_nome = f"uni_{identificador}"  # Adiciona "uni_" apenas se não estiver presente
+                    else:
+                        novo_nome = identificador  # Usa o identificador diretamente
+                    
+                    # Concatena o caminho do servidor com o nome da imagem
+                    caminho_completo = os.path.join(remote_folder, novo_nome)
+                    
+                    imagens.append((href, novo_nome))  # Armazena o link completo e o novo nome
+                    nomes_imagens.append(caminho_completo)  # Salva o caminho completo no banco de dados
+                    
+                    print("Link completo:", href)
+                    print("Novo nome da imagem:", novo_nome)
+                    print("Caminho completo para o banco de dados:", caminho_completo)
+
+            # Processa as imagens e salva os dados no banco de dados
             if cliente:
-                save_to_database(
+                process_images_and_save(
                     session=session,
                     cliente_id=cliente.id,
                     veiculo_1=si_veiculo_1,
@@ -246,39 +328,12 @@ for index, botao in enumerate(botoes_ver):
                     descricao_privada=descricao_privada,
                     descricao_publica=descricao_publica,
                     data_ocorrencia=data_ocorrencia,
-                    status=status
+                    status=status,
+                    image_links=imagens,  # Passa os links completos e os novos nomes
+                    nomes_imagens=nomes_imagens  # Passa os caminhos completos para salvar no banco
                 )
-
         else:
-            print("Dados do sinistro não encontrados.")
-
-
-        # # Localiza a aba anexos dentro do modal
-        # aba_anexos = driver.find_element(By.CSS_SELECTOR, 'li[data-id="anexos"]')
-        # aba_anexos.click()
-        # time.sleep(2)  # Aguarde o carregamento da aba
-        
-        # # Extrai o HTML da página
-        # html = driver.page_source
-        # # Analisa o HTML com BeautifulSoup
-        # soup = BeautifulSoup(html, 'html.parser')
-        
-        # # URL base do site para transformar os links relativos em absolutos
-        # base_url = "https://www.hitex.com.br"
-        
-        # # Localiza o contêiner com os anexos
-        # anexos_container = soup.find('div', class_='anexos_previews')
-
-        # # Captura os links das imagens dentro do contêiner de anexos
-        # if anexos_container:
-        #     links = anexos_container.find_all('a', class_='abrir_anexo')
-        #     imagens = [urljoin(base_url, link['href']) for link in links if 'href' in link.attrs]
-        #     for link in imagens:
-        #         print("Link encontrado:", link)
-        # else:
-        #     print("Nenhum anexo encontrado.")
-        
-        
+            print("Nenhum anexo encontrado.")
         
         # fecha modal
         pyautogui.click(x=1756, y=557)
@@ -286,8 +341,5 @@ for index, botao in enumerate(botoes_ver):
     except Exception as e:
       print(f"Erro ao processar sinistro {index}: {e}")
 
-
 # Fecha o navegador
 driver.quit()
-
-
